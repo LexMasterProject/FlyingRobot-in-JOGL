47e265deda76dbd8a6e9a91e211fa15ccd4b3423
AlexWei <wwei13@sheffield.ac.uk>
2014-11-14 03:21:48 +0000

    adjust robot orientation and floor

--- src/AppScene.java
+++ src/AppScene.java
@@ -82,14 +82,20 @@ public class AppScene {
       axes.display(gl, glut);
     
     drawRobot(gl);
+    displayFloor(gl);
 
   }
   
   
   private void drawRobot(GL2 gl)
   {
+	  gl.glPushMatrix();
+	  gl.glTranslated(-2, 4, -2);
+	  gl.glRotatef(45, 0, 1, 0);
+	  gl.glRotatef(45, 1, 0, 0);
+	 gl.glRotatef(-45, 0, 1, 0);
 	  
-	  gl.glTranslated(0, 2, 0);
+	 
 	  //draw head
 	  setHeadMaterialProperty(gl);
 	  int sphereslices = 100;
@@ -241,6 +247,10 @@ public class AppScene {
 	  		gl.glPopMatrix();
 	  	gl.glPopMatrix();
 	  gl.glPopMatrix();
+	  
+	  
+	  
+	  gl.glPopMatrix();
 	
   }
   	
@@ -330,6 +340,17 @@ public class AppScene {
 		  gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_SHININESS, matShininess, 0);
 		  gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_EMISSION, matEmission, 0);
   }
+  
+  private void displayFloor(GL2 gl) {
+	  float[] matAmbient = {0.2f, 0.6f, 0.2f, 1.0f};
+	  float[] matDiffuse = {0.2f, 0.6f, 0.2f, 1.0f};
+	  float[] matSpecular = {0.6f, 0.6f, 0.6f, 1.0f};
+	  float[] matShininess = {20.0f};
+	  float[] matEmission = {0.0f, 0.0f, 0.0f, 1.0f}; gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_AMBIENT, matAmbient, 0); gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, matDiffuse, 0); gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_SPECULAR, matSpecular, 0); gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_SHININESS, matShininess, 0); gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_EMISSION, matEmission, 0); gl.glPushMatrix(); // floor
+	        gl.glScaled(5,0.025,5);
+	        glut.glutSolidSphere(1.0f, 100,100);
+	      gl.glPopMatrix();
+	  }
   
   
 
