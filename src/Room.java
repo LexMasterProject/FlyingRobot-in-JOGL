40bcd5c8f57ac886b6f25062f458b1e4c89aa4b3
AlexWei <wwei13@sheffield.ac.uk>
2014-11-20 21:31:28 +0000

    load texture

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLProfile;
+
+import com.jogamp.opengl.util.awt.ImageUtil;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
 
 
 public class Room {
@@ -7,10 +16,15 @@ public class Room {
 	private Render floorRender;
 	private GL2 gl;
 	
+	private Texture floorTex;
+	
 	public Room(GL2 gl)
 	{
 		floorMesh = meshFactory.createPlane(40,40,40,40,1,1);
 		this.gl=gl;
+		
+		//load texture
+		floorTex=loadTexture(gl, "wattBook.jpg");
 	}
 	
 	public void prepareForRender()
@@ -21,8 +35,8 @@ public class Room {
 	
 	public void display()
 	{
-		//floorRender.renderDisplayList(gl);
-		floorRender.wireframeImmediateMode(gl, true);
+		floorRender.renderDisplayList(gl);
+		//floorRender.wireframeImmediateMode(gl, true);
 	}
 	public Mesh getFloor() {
 		return floorMesh;
@@ -31,6 +45,20 @@ public class Room {
 	public void setFloor(Mesh floorMesh) {
 		this.floorMesh = floorMesh;
 	}
+	
+	
+	private Texture loadTexture(GL2 gl, String filename) { Texture tex = null;
+	try {
+	File f = new File(filename);
+	// tex = TextureIO.newTexture(new File(filename), false);
+	BufferedImage img = ImageIO.read(f); ImageUtil.flipImageVertically(img);
+	tex = AWTTextureIO.newTexture(GLProfile.getDefault(), img, false);
+	tex.setTexParameteri(gl, GL2.GL_TEXTURE_MAG_FILTER, GL2.GL_NEAREST);
+	tex.setTexParameteri(gl, GL2.GL_TEXTURE_MIN_FILTER, GL2.GL_NEAREST); }
+	catch(Exception e) {
+	System.out.println("Error loading texture " + filename);
+	}
+	return tex; }
 	
 	
 }
